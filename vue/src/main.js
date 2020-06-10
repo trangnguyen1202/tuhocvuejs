4bbd8f521d40793107769da84eb2f093b22e8811
trangnguyen1202 <trangnguyen12021995@gmail.com>
2020-06-10 16:45:28 +0700

    day lai project vuejs

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
